eaa0ec2a54c66f2d4c5bd1b7733f04dbbeadc14c
awesomund <osmund@iterate.no>
2014-06-27 15:46:48 +0200

    pleh

--- webapp/sportsfronter/settings.py
+++ webapp/sportsfronter/settings.py
@@ -297,14 +297,15 @@ LOGGING = {
 }
 
 #Settings for Heroku:
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-CurDir = os.path.dirname(os.path.abspath(__file__))
-ProjectDir = os.path.join(CurDir, "sportsfronter")
-import sys
-sys.path += [ProjectDir]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 try:
     from settings_dev import *
